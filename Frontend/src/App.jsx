68f64c96eaf1b39634de6d62756585dee06ba9a9
Nikola <camnesya@gmail.com>
2025-09-09 13:33:51 +0200

    Set up initial project structure for frontend!

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <div>
+        <h1>This is App!</h1>
+      </div>
+    </main>
+  );
+}
+export default App;
---
+++ README.md
@@ -0,0 +1,1 @@
+CritiCrew is movie platform where users can explore, rate and comment on movies.
