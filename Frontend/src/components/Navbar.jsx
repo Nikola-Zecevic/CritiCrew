9f5f0131edc6a22f0f437acf664a753cb3303a05
Branko3 <branko.raickovic3211@gmail.com>
2025-09-11 13:48:40 +0200

    Fixed merge bugs.

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-// src/components/Navbar.jsx
-import React from "react";
-import { Link } from "react-router-dom";
-import "../styles/Navbar.css";
-
-function Navbar() {
-  const user = { name: "Jane Doe" };
-
-  function getInitials(name) {
-    return name
-      .split(" ")
-      .map((word) => word[0])
-      .join("")
-      .toUpperCase();
-  }
-
-  return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
-          MyApp
-        </Link>
-
-        <ul className="nav-menu">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/custom" className="nav-link">
-              Custom Page
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about" className="nav-link">
-              About Us
-            </Link>
-          </li>
-        </ul>
-
-        <div className="nav-profile">
-          <button className="avatar-button">{getInitials(user.name)}</button>
-        </div>
-      </div>
-    </nav>
-  );
-}
-
-export default Navbar;
-=======
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../styles/Navbar.css";
@@ -148,4 +96,3 @@ function Navbar() {
 }
 
 export default Navbar;
->>>>>>> 02a6fb1c0da4da51c18637bbc8bfab992add5ee1
