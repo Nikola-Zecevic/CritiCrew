e8fa1dc9a398801f61109be9a09a0ed49a34e536
Nikola <camnesya@gmail.com>
2025-09-12 12:49:48 +0200

    Uploaded right version of project!

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,22 @@
-// src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Custom from "./pages/Custom";
+// // src/App.jsx
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Layout from "./components/Layout";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Custom from "./pages/Custom";
 
-function App() {
-  return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/custom" element={<Custom />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Layout>
-    </Router>
-  );
-}
+// function App() {
+//   return (
+//     <Router>
+//       <Layout>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/custom" element={<Custom />} />
+//           <Route path="/about" element={<About />} />
+//         </Routes>
+//       </Layout>
+//     </Router>
+//   );
+// }
 
-
-export default App;
-
+// export default App;
