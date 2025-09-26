b650444cc43706579c6c8137ed942c42f82dba03
Branko3 <branko.raickovic3211@gmail.com>
2025-09-26 16:03:49 +0200

    Protected review routes.

--- Backend/models/review.py
+++ Backend/models/review.py
@@ -1,22 +1,17 @@
 from datetime import datetime
 from typing import Optional, TYPE_CHECKING
 from sqlmodel import SQLModel, Field, Relationship
-
 if TYPE_CHECKING:
     from .user import User
     from .movie import Movie
-
 class Review(SQLModel, table=True):
     __tablename__ = "reviews"
-
     id: Optional[int] = Field(default=None, primary_key=True)
     rating: int
     review_text: str
     review_date: datetime = Field(default_factory=datetime.now)
-    
     # Foreign key fields
     user_id: Optional[int] = Field(default=None, foreign_key="users.id")
     movie_id: Optional[int] = Field(default=None, foreign_key="movies.id")
-
     user: Optional["User"] = Relationship(back_populates="reviews")
     movie: Optional["Movie"] = Relationship(back_populates="reviews")
